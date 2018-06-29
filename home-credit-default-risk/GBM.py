2ccf397f89b9b8dc9f21d64e1441e2e6c5e9b726
DeanN <dean_j_ninalga@hotmail.com>
2018-06-29 11:37:20 +0000

    Fixed hanging gbm bug

--- home-credit-default-risk/GBM.py
+++ home-credit-default-risk/GBM.py
@@ -4,12 +4,12 @@ from kaggleProjects.directory_table import get_paths
 from kaggleProjects.SequentialParamSearch import sequential_search
 
 
-data_dir, pkl_dir = get_paths(station='Windows')
+data_dir, pkl_dir = get_paths(station='Subgraph')
 app_train_df = pd.read_csv(data_dir + '/application_train.csv')
 len_train = len(app_train_df)
 del app_train_df
-meta = pd.read_pickle(pkl_dir + r'\meta_df.pkl')
-train = pd.read_csv(pkl_dir + '/train.csv', nrows=len_train)
+meta = pd.read_pickle(pkl_dir + '/meta_df.pkl')
+train = pd.read_csv(pkl_dir + '/train.csv', nrows=100)
 target = 'TARGET'
 print(train.columns)
 predictors = list(set(train.columns) - set(meta.columns) - {target, 'Unnamed: 0'})
@@ -20,11 +20,12 @@ lgbm_const_params = {
 }
 param_group1 = [
     {
-        'n_estimators': list(range(20, 101, 10))
+        'n_estimators': [1]
     },
     {
         'max_depth': list(range(5, 16)),
-        'min_data_per_group': list([20, 100, 250, 500, 750, 1000])
+        'num_leaves': [pow(2, x) for x in range(5, 16)],
+        'min_data_per_group': [20, 100, 250, 500, 750, 1000]
     },
     {
         'min_data_in_leaf': list(range(30, 101, 10))
@@ -46,6 +47,6 @@ optimal_params = sequential_search(classifier_algo=LGBMClassifier,
                                    predictors=predictors,
                                    target=target,
                                    scoring='roc_auc',
-                                   n_jobs=3,
+                                   n_jobs=1,
                                    verbose=True)
 print(optimal_params)
